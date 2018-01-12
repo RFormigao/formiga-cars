5d922215096b3f0b724d49f7d7f2ecd121ca5140
Robson Formigão <rformigao.gomes@gmail.com>
2018-01-12 16:49:19 -0200

    remove empty register

--- challenge-30.js
+++ challenge-30.js
@@ -9,19 +9,16 @@
         this.companyInfo();
         this.initEvents();
       },
-
       initEvents: function initEvents() {
         var $formVehicle = $('[data-js="form-insert-vehicle"]');
         $formVehicle.on('submit', this.handleFormVehicles);
       },
-
       companyInfo: function companyInfo() {
         var ajax = new XMLHttpRequest();
         ajax.open('GET', 'https://raw.githubusercontent.com/RFormigao/curso-javascript-ninja/master/challenge-29/company.json');
         ajax.send();
         ajax.addEventListener('readystatechange',this.getInfoCompany, false);
       },
-
       getInfoCompany: function getInfoCompany() {
         if (app().isRequestOk.call(this)) {
           var data   =  JSON.parse(this.responseText);
@@ -29,38 +26,31 @@
           $('[data-js="number"]').get()[0].textContent = data.phone;
         }
       },
-
       isRequestOk: function isRequestOk() {
         return this.readyState === 4 && this.status === 200;
       },
-
       handleFormVehicles: function handleFormVehicles(event) {
         event.preventDefault();   
-        var $formFields = $('[data-js="input-field"]');        
+        var $formFields = $('[data-js="input-field"]');   
+        app().removeFirstLine();     
         app().addVehicle.call($formFields);
         $formFields.methodArray('forEach', app().clearFields);     
       },
-
       addVehicle: function addVehicle() {
         var table = app().table();
-
         this.get().forEach( function (item, index, array) {
           table.columnTable = doc.createElement("td"); 
-          
           if ( array[index] === array[0] ) {
             table.imageTable.setAttribute ( "src", array[0].value );
             table.columnTable.appendChild(table.imageTable );
             table.lineTable.appendChild(table.columnTable ); 
             return; 
-          } 
-          
+          }      
           table.columnTable.appendChild( doc.createTextNode( item.value ) );
-          table.lineTable.appendChild(table.columnTable );            
+          table.lineTable.appendChild(table.columnTable );   
         });
-       
         table.bodyTable.get()[0].appendChild( table.lineTable );   
       },
-
       table: function table() {
         return {
           bodyTable   : $('[data-js="table-body"]'),
@@ -68,10 +58,14 @@
           imageTable  : doc.createElement("img"),
           columnTable : doc.createElement("td")
         }
-      },
-      
+      },      
       clearFields: function clearFields(item) {
         item.value = '';
+      }, 
+      removeFirstLine: function removeFirstLine() {
+        var lineTable = $('[data-js="empty-register"]');
+        var element = lineTable.get()[0].children[0];
+        lineTable.get()[0].removeChild(element);     
       }
     }
   }
