984f02b9fc8d98aa0ffb6fb4361f7cd2148d4bd4
Robson <robson.formigao.gomes@hotmail.com>
2018-01-16 00:19:34 -0200

    Alterei o app.js adicionando a funcionalide de remover um veiculo da tabela

--- app.js
+++ app.js
@@ -39,6 +39,7 @@
         var $formFields = $('[data-js="input-field"]');   
         app().removeFirstLine();     
         app().addVehicle.call($formFields);
+        app().optionsVehicle();
         $formFields.methodArray('forEach', app().clearFields);     
       },
 
@@ -58,14 +59,38 @@
         table.bodyTable.get()[0].appendChild( table.lineTable );   
       },
 
+      removeVehicle: function removeVehicle(event) {
+        event.preventDefault();
+        var tr = this.parentNode;
+        var $table = app().table();
+        $table.bodyTable.get()[0].removeChild(tr);
+        
+        if ($table.bodyTable.get()[0].children.length === 1) {
+          var lineTable = $('[data-js="empty-register"]');
+          var element = lineTable.get()[0].children[0];
+          element.setAttribute ( "style", "display:table-cell;text-align:center; padding: 100px; text-transform:uppercase;" );
+        }
+      },
+
       table: function table() {
         return {
-          bodyTable   : $('[data-js="table-body"]'),
-          lineTable   : doc.createElement("tr"),
-          imageTable  : doc.createElement("img"),
-          columnTable : doc.createElement("td")
+          bodyTable         : $('[data-js="table-body"]'),
+          lineTable         : doc.createElement("tr"),
+          imageTable        : doc.createElement("img"),
+          columnTable       : doc.createElement("td"),
+          optionRemoveTable : '<td data-js="remove-vehicle" > <a href="#"> Remover veiculo </a> </td>' 
         }
-      },    
+      },
+      
+      optionsVehicle: function optionsVehicle() {
+        var table = app().table();
+        var lastVehicle = table.bodyTable.element[0].lastChild;
+        lastVehicle.insertAdjacentHTML('beforeend', table.optionRemoveTable);
+        var self = this;
+        var $removeVehicle = $('[data-js="remove-vehicle"]'); 
+        var lastIndex = $removeVehicle.element.length - 1;
+        $removeVehicle.get()[lastIndex].addEventListener('click', self.removeVehicle, false);
+      },
 
       clearFields: function clearFields(item) {
         item.value = '';
@@ -74,7 +99,7 @@
       removeFirstLine: function removeFirstLine() {
         var lineTable = $('[data-js="empty-register"]');
         var element = lineTable.get()[0].children[0];
-        lineTable.get()[0].removeChild(element);     
+        element.setAttribute ( "style", "display:none;" );
       }
       
     }
